1ee2aa8bbead428f551619aafcf5468fe2814bd4
Abushahba <33253332+Abushahba@users.noreply.github.com>
2017-12-06 12:14:50 +0200

    Data wrangling part of exercise 5
    part 1 of exercise 5

--- data/create_human.R
+++ data/create_human.R
@@ -48,3 +48,51 @@ human <- hd_gii
 
 write.csv(human, file="C:/Users/ahmed/Desktop/IODS-project/data/human.csv")
 
+# To complete the data wrangling part,
+# we will continue working on my create_human.R from last week 
+# transform the Gross National Income (GNI) variable to numeric
+human <- read.csv(file = "C:/Users/ahmed/Desktop/IODS-project/data/human.csv")
+human <- human[-1]
+
+str(human$GNI)
+
+# removing commas from GNI and make it numeric
+
+str_replace(human$GNI, pattern=",", replace ="")
+
+is.factor(human$GNI)
+
+human$GNI = as.numeric(human$GNI)
+is.numeric(human$GNI)
+
+# Exclude unneeded variables: keep only the columns matching the following
+# variable names:  "Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F" 
+
+keep_columns <- c("country", "popu_sec_edu_fem", "lab_force_partic_femal", "exp_year_edu", "life_exp_birth", "GNI", "MMR", "ABR", "parl_repres")
+human <- dplyr::select(human, one_of(keep_columns))
+
+# Remove all rows with missing values and remove
+# the observations which relate to regions instead of countries
+complete.cases(human)
+
+data.frame(human, comp = complete.cases(human))
+human_mut <- mutate(human, comp = complete.cases(human))
+dim(human_mut)
+
+human_mut <- filter(human, human_mut$comp == TRUE)
+human_ <- human_mut
+
+tail(human_, n=10)
+humanW <- human_ [1:155,]
+
+# Define the row names of the data by the country names
+# and remove the country name column from the data. 
+
+rownames(humanW) <- humanW$country
+humanW <- dplyr::select(humanW, -country)
+
+# Save the human data in your data folder 
+# including the row names. You can overwrite your old 'human' data
+
+write.csv(humanW, file = "C:/Users/ahmed/Desktop/IODS-project/data/human.csv")
+
